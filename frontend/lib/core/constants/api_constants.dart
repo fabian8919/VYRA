b7679c81baf81cdacb8a98ae604f202fc8c0f75b
pipe9494 <49037321+pipe9494@users.noreply.github.com>
2026-05-24 22:59:03 -0500

    kk

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -6,6 +6,7 @@ class ApiConstants {
   // static const String baseUrl = 'http://10.0.2.2:3000'; // Android emulator
   // static const String baseUrl = 'http://localhost:3000'; // iOS simulator / web
 
+
   static const String apiVersion = '/api';
 
   // Auth endpoints
